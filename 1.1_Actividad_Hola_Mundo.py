493b1ce45051d58973b384769dd2a3f8e8d7062f
DiegArre <diego.arredo18@gmail.com>
2021-06-05 12:25:24 -0400

    Commit 2 primeras actividades Python fundamentals

---
+++ 1.1_Actividad_Hola_Mundo.py
@@ -0,0 +1,37 @@
+# 1. TAREA: imprimir "Hola mundo"
+print("Hola mundo")
+# 2. imprimir "Hola Noelle!" con el nombre en una variable
+name = "Noelle"
+print( "Hola",name,"!")	# con una coma
+print("Hola "+ name + "!")	# con un +
+# 3. imprimir "Hola 42!" con un numero en una variable
+name = 42
+print( "Hola",name,"!" )	# con una coma
+print( "Hola "+ name + "!")	# con un + - !Este debería darnos un error!
+# 4. imprimir "Me encanta comer sushi y pizza." con los alimentos en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("Me encanta comer {} y {}.".format(fave_food1,fave_food2)) # con .format()
+print(f"Me encanta comer {fave_food1} y {fave_food2}.")# con una cadena f
+
+
+#PARTE 2
+
+nombre = "Diego"
+print("Hola",nombre)
+print("Hola " + nombre)
+num = 7
+print("Hola",num)
+print("Hola " + num) # error pq no es string y hace math.
+#Para hacer funcionar lo anterio lo guardamos como string el numero:
+numS = "7"
+print("Hola " + numS)# :D
+Comida_fav1 = "Pollo frito"
+Comida_fav2 = "Sushi"
+print("Me encanta comer {} y {}.".format(Comida_fav1,Comida_fav2)) # con .format()
+print(f"Me encanta comer {Comida_fav1} y {Comida_fav2}.")# con una cadena f
+
+
+
+
+
---
+++ Ejercicios_funciones.py
@@ -0,0 +1,66 @@
+# Ejercicios en clase
+
+x = "HoLa"
+y = x[::-1] #PYTHON SLICE STRING
+z = x.lower()
+print(x[:4])
+print(y)
+print(z)
+print (len(x))
+
+
+# Crea una funcion que dado una palabra diga si es palindroma o no.
+def Palindromo(pal):
+    pal = pal.lower()
+    inv = pal[::-1]
+    if pal == inv:
+        print("OOhh!! Es Palindromaa!")
+        print(f"""Palabra: {pal}
+Invertida: {inv}""")
+    else:
+        print("No es palindroma")
+
+Palindromo("Reconocer")        
+
+# - Crea una función que tome una lista y devuelva el primer y el último valor de la lista. Si la longitud de la lista es menor que 2, haga que devuelva False.
+
+def devolver(lista = []): #AL ASIGNARLE VALOR = [], PERMITE QUE AL NO DARLE PARAMETROS A LA FUNCION, NO TIRA ERROR Y CONTINUA
+    if len(lista) >= 2:
+        x = lista[0]
+        y = lista[len(lista)-1]  # y = lista[:len(lista)]  SLICE STRING
+        print("Primero:", x)
+        print("Ultimo:", y)
+        return x,y
+    else:
+        print("La longitud es menor que dos")
+    return False, False #Asi en la linea 41 se define bien ambas variables
+
+ejemplo = devolver([1]) #EL RETURN CREA UNA TUPLA
+
+print("tupla =", ejemplo)
+primero,ultimo = ejemplo
+print(f"El primero es {primero}, y el ultimo es {ultimo}")
+
+
+# - Crea una función que tome una lista y devuelva un diccionario con su mínimo, máximo, promedio y suma.
+
+def Diccionario(lista):
+    minimo = min(lista)
+    maximo = max(lista)
+    suma = sum(lista)
+    promedio = sum(lista)/len(lista)
+    dic = {"Minimo" : minimo, "Maximo": maximo,"Suma": suma, "Promedio": promedio}
+    return dic
+print(Diccionario([1,2,3,4,5,6,7,8,9,10]))
+
+def Diccionario(lista):
+    dic = {}
+    minimo = min(lista)
+    dic["Minimo"] = minimo
+    maximo = max(lista)
+    suma = sum(lista)
+    promedio = sum(lista)/len(lista)
+    
+    return dic
+print(Diccionario([1,2,3,4,5,6,7,8,9,10]))
+
