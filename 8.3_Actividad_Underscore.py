86d851bdc5fb71b78008a643ffab93a7d6d6ea77
DiegArre <diego.arredo18@gmail.com>
2021-06-11 22:36:03 -0400

    Se mejoran algunas funciones

--- 8.3_Actividad_Underscore.py
+++ 8.3_Actividad_Underscore.py
@@ -10,25 +10,27 @@ class Underscore:
     def find(self, iterable, callback):
 
         for num in iterable:
-            if callback(num) == True:
-                return num
-            else:
-                return False
+            # print(callback(num))
+            if callback(num): #== True:
+                 return num
+
 
 
     def filter(self, iterable, callback):
         temp = []
         for num in iterable:
-            if callback(num) == True:
+            if callback(num): #== True:
                 temp.append(num)
         return temp
 
+    def filter2(self, iterable, callback):
+        return [x for x in iterable if callback(x)]
 
     def reject(self, iterable, callback):
 
         temp = []
         for num in iterable:
-            if callback(num) == True:
+            if callback(num): #== True:
                 continue
             else:
                 temp.append(num)
@@ -42,8 +44,10 @@ evens = _.filter([1, 2, 3, 4, 5, 6], lambda x: x % 2 == 0)
 # debe retornar [2, 4, 6] después que termines de implementar el código de arriba
 
 print(_.map([1,2,3], lambda x: x*2)) # debe retornar [2,4,6]
-print(_.find([1,2,3,3,4,4], lambda x: x>4)) # debe retornar el primer valor que es mayor que 4
+print(_.find([7,5,3,3,6,4], lambda x: x>4)) # debe retornar el primer valor que es mayor que 4
 print(_.filter([1,2,3,4,5,6], lambda x: x%2==0)) # debe retornar [2,4,6]
+print(_.filter2([1,2,3,4,5,6], lambda x: x%2==0)) # debe retornar [2,4,6]
+
 print(_.reject([1,2,3,4,5,6], lambda x: x%2==0)) # debe retornar [1,3,5]
 
 
